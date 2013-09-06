a2b620e0d261dc0157569efeaf4b925eace7b631
Scott Reynolds <scott@scottreynolds.us>
2013-09-06 12:01:11 -0700

    little more work on how I think it should work

--- src/main.c
+++ src/main.c
@@ -2,34 +2,11 @@
 #include "server.h"
 #include "dbg.h"
 #include <ev.h>
-#include "adt/hash.h"
 
 #define UDP_BUF_LEN 2048
 
-hash_t *servers;
-
-int compare_fds (const void *fd_left, const void *fd_right);
-uint32_t hash_fd (const void *fd);
-
-int compare_fds (const void *fd_left, const void *fd_right)
-{
-    int left_value = *((int *) fd_left);
-    int right_value = *((int *) fd_right);
-
-    if (left_value == right_value){
-	return 0;
-    }
-    if (left_value < right_value) {
-	return -1;
-    }
-    return 1;
-}
-
-uint32_t hash_fd (const void *fd)
-{
-    uint32_t fd_value = *((uint32_t *) fd);
-    return fd_value & HASH_VAL_T_MAX;
-}
+int server_count;
+server *servers;
 
 static void udp_callback(EV_P_ ev_io *w, int revents)
 {
@@ -40,10 +17,14 @@ static void udp_callback(EV_P_ ev_io *w, int revents)
     int bytes_read = recv(w->fd, buffer, UDP_BUF_LEN - 1, 0);
     check (bytes_read >= 0, "Failed to read bytes");
 
-    hnode_t *node = hash_lookup(servers, &w->fd);
-    check (node != NULL, "Failed to get the server for: %d", w->fd);
+    server *server = NULL;
+    for (int idx = 0; idx < server_count; idx++) {
+	if (servers[idx].socket_descriptor == w->fd) {
+	    server = &servers[idx];
+	}
+    }
+    check(server != NULL, "Server not found for fd: %d", w->fd);
 
-    server *server = hnode_get(node);
     log_info("Server found: %d", server->port);
     syslog_parser *parser = syslog_parser_init();
     syslog_parser_execute(parser, buffer, bytes_read, 0);
@@ -57,13 +38,13 @@ static void udp_callback(EV_P_ ev_io *w, int revents)
 
 int main ()
 {
-    servers = hash_create(10, (hash_comp_t) compare_fds,(hash_fun_t) hash_fd);
     server *server = server_init(1099);
     debug("Server port is: %d", server->port);
     int bind_result = server_bind(server);
     check(bind_result == 0, "Failed to bind: %d", bind_result);
 
-    hash_alloc_insert(servers, &server->socket_descriptor, server);
+    server_count = 1;
+    servers = server;
 
     struct ev_loop *loop = EV_DEFAULT;
     ev_io udp_watcher;
